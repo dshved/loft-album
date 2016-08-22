fbef4cb266bdfd178074c3b0f53c59eb0a7f6143
Dmitry Shved <dshved@hotmail.com>
2016-08-22 15:05:14 +0300

    Fixed a bug when editing photos

--- app/controllers/addphoto.js
+++ app/controllers/addphoto.js
@@ -32,31 +32,36 @@ router.post('/addphoto', function(req, res, next) {
     type = fields.type_modal[0];
     description = fields.photo_description[0];
     title = fields.photo_name[0];
-    if (!isEmptyObject(files)) {
-    if (type === 'add') {
-      album_id = fields.album_id;
-      filePath = files.upload_bg[0].path;
-      fileSize = files.upload_bg[0].size;
-      fs.readFile(filePath, function(err, data) {
-        var radom = Math.random().toString(36);
-        var randomName = radom.substring(2, radom.length);
-        var path = './public/upload/' + randomName + '-' + files.upload_bg[0].originalFilename;
-        fs.writeFile(path, data, function(err) {
-          Photo.find({}, function(err, photo) {
-            if (err) return next(err);
-            var newPhoto = new Photo({
-              album: album_id,
-              description: description,
-              title: title,
-              file: '/upload/' + randomName + '-' + files.upload_bg[0].originalFilename
-            });
-            newPhoto.save();
-            res.redirect('/main/album');
-            res.end();
-          });
-        });
 
-      });
+    if (type === 'add') {
+      if (!isEmptyObject(files)) {
+        album_id = fields.album_id;
+        filePath = files.upload_bg[0].path;
+        fileSize = files.upload_bg[0].size;
+        fs.readFile(filePath, function(err, data) {
+          var radom = Math.random().toString(36);
+          var randomName = radom.substring(2, radom.length);
+          var path = './public/upload/' + randomName + '-' + files.upload_bg[0].originalFilename;
+          fs.writeFile(path, data, function(err) {
+            Photo.find({}, function(err, photo) {
+              if (err) return next(err);
+              var newPhoto = new Photo({
+                album: album_id,
+                description: description,
+                title: title,
+                file: '/upload/' + randomName + '-' + files.upload_bg[0].originalFilename
+              });
+              newPhoto.save();
+              res.redirect('/main/album');
+              res.end();
+            });
+          });
+
+        });
+      } else {
+        res.redirect('/main/album');
+        res.end();
+      }
     };
     if (type === 'edit') {
       photo_id = fields.photo_id[0];
@@ -71,16 +76,12 @@ router.post('/addphoto', function(req, res, next) {
         });
       });
     };
-  } else {
-    res.redirect('/main/album');
-    res.end();
-  }
   });
 });
 
 router.post('/delphoto', function(req, res, next) {
   var photo_id = req.body.photo_id;
-  Photo.findByIdAndRemove( photo_id, function(err) {
+  Photo.findByIdAndRemove(photo_id, function(err) {
     if (err) return next(err);
     res.redirect('/main/album');
     res.end();
